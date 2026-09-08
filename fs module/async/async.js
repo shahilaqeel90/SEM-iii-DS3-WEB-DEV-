94ffefd1de1d5ae4539686200af4fc834930181b
shahilaqeel90 <shahil.25b15410307@abes.ac.in>
2026-09-08 14:40:39 +0530

    async.js

---
+++ fs module/async/async.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+fs.writeFileSync(
+    'example.txt',
+    'This is Experiment 2 in FSD Workshop',
+    'utf8'
+);
+
+fs.writeFileSync(
+    'example1.txt',
+    'This is Experiment 2 in FSD Workshop',
+    'utf8'
+);
+
+console.log('create file run successfully');
+
+const data = fs.readFileSync('example.txt', 'utf8');
+
+console.log('file content is:', data);
+
+fs.appendFileSync('example.txt', '\nThis is the new line');
+
+console.log('Example file is appended successfully');
+
+fs.unlinkSync('example1.txt');
+
+console.log('example1 file is deleted successfully');
+fs.mkdirSync('new-directory');
+console.log('new directory created successfully');
+fs.rmdirSync('new-directory');
+console.log('new directory deleted successfully');
+if(fs.existsSync('example.txt')){
+    console.log('file exists');
+}else{
+    console.log('file doesnot exists');
+}
